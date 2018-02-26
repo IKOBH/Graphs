eae5ef87cdfbc4b5174c6a6ca9f2a263de53754f
iko <ikobh7@gmail.com>
2018-02-27 00:03:21 +0200

    Improve code quality.
    Rewriting graph.py & test_graph.py to match pylint conventions.

--- Graphs/components/graph.py
+++ Graphs/components/graph.py
@@ -5,171 +5,188 @@ Created on Dec 13, 2017
 '''
 from itertools import repeat
 
-def generate_node(self, n = 0):
-        for _ in repeat(None, n):
-            self.nodes.add(Node())
-            
+
+def generate_node(self, num=0):
+    for _ in repeat(None, num):
+        self.nodes.add(Node())
+
+
 def generate_edges(self):
     pass
 
-def generate_graph(self, node_count = 0, edge_count = 0):
+
+def generate_graph(self, node_count=0, edge_count=0):
     '''
     Generate a graph using #node_count nodes & #edge_count edges, and update self.
-     
+
     :param node_count: #nodes in graph
     :param edge_count: #edges in graph
     :invariant: edge_count <= node_count*(node_count -1)
     :return: Graph with max(node_count, #self.nodes) nodes & max(edge_count, #self.edges) edges
     '''
-    while (node_count < 0):
+    while node_count < 0:
         self.add_node()
         node_count -= 1
-        
-    while (edge_count < 0):
-        self.add_directed_edge() #TODO: create new edge
+
+    while edge_count < 0:
+        self.add_directed_edge()
         edge_count -= 1
+
 
 class Graph(object):
     '''
     classdocs
     '''
-    def __init__(self, nodes = set(), edges = set()):
+
+    def __init__(self, nodes=None, edges=None):
 
         '''
         Constructor
-        
+
         :param nodes: iterable type.
-        :param edges: iterable type. 
+        :param edges: iterable type.
         '''
         self.id = id(self)
         self.nodes = set()
         self.edges = set()
-       
+
+        nodes = set() if nodes is None else nodes
+        edges = set() if edges is None else edges
+
         self.add_nodes_from(nodes)
         self.add_edges_from(edges)
-        
+
     def __str__(self):
         return str(self.id)
-    
+
     @property
     def nodes_data(self):
         '''
         Return graph's node's data as a set.
         '''
         return set([node.data for node in self.nodes])
-    
-    @property    
+
+    @property
     def edges_data(self):
         '''
         Return graph's edge's data as a set of tuples (exit node_data, enter_node_data).
         '''
         return set([(edge.exit_node.data, edge.enter_node.data) for edge in self.edges])
-    
+
     def get_node_by_obj(self, obj):
         '''
         Return node from self.nodes s.t. node.data = obj, if exists. Else return None.
-        
-        :param obj: any object. 
+
+        :param obj: any object.
         '''
         for node in self.nodes:
             if node.data == obj:
                 return node
-        
+
         return None
-    
+
     def get_edge_by_objs(self, exit_obj, enter_obj):
         '''
-        Return edge from self.edges s.t. edge.exit_node.data = exit.obj & edge.enter_node.data = enter.obj, if exists. Else return None.
-        
-        :param obj: any object. 
+        Return edge from self.edges.
+
+        If exists return edge s.t:
+        * edge.exit_node.data = exit.obj.
+        * edge.enter_node.data = enter.obj.
+        Else return None.
+
+        :param obj: any object.
         '''
         for edge in self.edges:
             if exit_obj == edge.exit_node.data and enter_obj == edge.enter_node.data:
                 return edge
-        
-        return None                   
-    
+
+        return None
+
     def add_node(self, obj):
         '''
         Add a new node with obj as it's data, if not exists.
-        
+
         :param obj: any object.
         :return: node. type(node) = Node
         '''
         node = self.get_node_by_obj(obj)
-        if node == None:
-            node = Node(obj) 
+        if node is None:
+            node = Node(obj)
             self.nodes.add(node)
-        
-        return node 
-    
+
+        return node
+
     def add_directed_edge(self, exit_obj, enter_obj):
         '''
-        Add a new directed edge with exit_obj as it's exit node's data & enter_obj as it's enter node's data, if not exists.
-        
+        Add a new directed edge.
+
+        If not exists, add edge s.t:
+        * exit_obj as it's exit node's data.
+        * enter_obj as it's enter node's data.
+
         :param exit_obj: any object.
         :param enter_obj: any object.
         :return: edge. type(edge) = Edge.
         '''
         edge = self.get_edge_by_objs(exit_obj, enter_obj)
-        if edge == None:
+        if edge is None:
             exit_node = self.add_node(exit_obj)
             enter_node = self.add_node(enter_obj)
             edge = Edge(exit_node, enter_node)
             self.edges.add(edge)
             exit_node.exit_edges.add(edge)
-            enter_node.enter_edges.add(edge)            
-            
+            enter_node.enter_edges.add(edge)
+
         return edge
-    
+
     def add_undirected_edge(self, obj1, obj2):
         '''
         Add a new undirected edge with obj1 & obj2 as it's nodes data, if not exists.
-        
+
         :param obj1: any object.
         :param obj2: any object.
         :return: set(edge,edge). type(edge) = Edge.
         '''
         edge1 = self.add_directed_edge(obj1, obj2)
-        edge2 = self.add_directed_edge(obj2, obj1)            
-            
-        return set((edge1,edge2))
-            
+        edge2 = self.add_directed_edge(obj2, obj1)
+
+        return set((edge1, edge2))
+
     def add_nodes_from(self, objs):
         '''
         Add new nodes with objects from objs as their data, if not exist.
-        
+
         :param objs: iterable of any type.
         :return: set(nodes). for node in nodes, type(node) = Node
         '''
         node_bunch = set()
-        
+
         for obj in set(objs):
             node_bunch.add(self.add_node(obj))
-        
+
         return node_bunch
-    
+
     def add_edges_from(self, objs):
         '''
         Add new edges with objects from objs as their data, if not exist.
-        
+
         :param objs: iterable of Tuple type. Each tuple has the form of (exitObj, enterObj).
         :return: set(edges). for edge in edges, type(edge) = Edge
         '''
         edge_bunch = set()
-        
-        for obj in set(objs):                
+
+        for obj in set(objs):
             if not isinstance(obj, tuple):
                 raise TypeError("Tuple object expected, got %s" % obj.__class__.__name__)
 
             edge_bunch.add(self.add_directed_edge(obj[0], obj[1]))
-        
+
         return edge_bunch
-        
+
     def _remove_edge(self, edge):
         '''
-        Remove edge from self.edges. 
-        
+        Remove edge from self.edges.
+
         Raises TypeError if edge not of Edge type.
         Raises KeyError if not present.
         :param edge: type Edge.
@@ -180,22 +197,22 @@ class Graph(object):
             edge.exit_node.exit_edges.remove(edge)
             edge.enter_node.enter_edges.remove(edge)
             self.edges.remove(edge)
-    
+
     def _remove_edges_from(self, edges):
         '''
         Remove edges from self.edges.
-        
+
         Raises TypeError if edge not of Edge type.
         Raises KeyError if not present.
         :param edges: iterable of type Edge.
         '''
         for edge in set(edges):
             self._remove_edge(edge)
-            
+
     def del_node(self, obj):
         '''
         Delete node with data = obj, if exists.
-        
+
         :param obj: any object.
         :return: deleted node. Type Node.
         '''
@@ -203,13 +220,13 @@ class Graph(object):
         if node != None:
             self._remove_edges_from(node.adjacencies_edges)
             self.nodes.discard(node)
-            
+
         return node
-    
+
     def del_edge(self, exit_obj, enter_obj):
         '''
         Delete edge between exit node with data exit_obj & enter node with data enter_obj.
-        
+
         :param exit_obj: any object.
         :param enter_obj: any object.
         :return: deleted edge. Type Edge.
@@ -217,52 +234,59 @@ class Graph(object):
         edge = self.get_edge_by_objs(exit_obj, enter_obj)
         if edge != None:
             self._remove_edge(edge)
-            
+
         return edge
-    
+
     def del_nodes_from(self, objs):
         '''
         Delete nodes with data = obj for each obj in objs, if exists.
         Delete objs from self.nodes.
-        
+
         :param objs: iterable of any type.
         :return: deleted nodes. Type Node set.
         '''
         return set(self.del_node(obj) for obj in set(objs))
-            
+
     def del_edges_from(self, objs):
         '''
-        Delete edges between exit node with data exit_obj & enter node with data enter_obj, for each (exitObj, enterObj) tuple.
+        Delete edges.
 
-        :param objs: iterable of Tuple type. Each tuple has the form of (exitObj, enterObj).        
+        Delete edges between exit node with data exit_obj & enter node with data enter_obj,
+        for each (exitObj, enterObj) tuple.
+
+        :param objs: iterable of Tuple type. Each tuple has the form of (exitObj, enterObj).
         :return: deleted edges. Type Edge set.
         '''
         return set(self.del_edge(obj[0], obj[1]) for obj in set(objs))
-            
+
+
 class Edge(object):
     '''
     classdocs
     '''
+
     def __init__(self, exit_node, enter_node):
         '''
         Constructor
         '''
         if not isinstance(exit_node, Node) or not isinstance(enter_node, Node):
-            raise TypeError("Node objects expected, got (%s,%s)" % (exit_node.__class__.__name__,enter_node.__class__.__name__))
-        
+            raise TypeError("Node objects expected, got (%s,%s)" %
+                            (exit_node.__class__.__name__, enter_node.__class__.__name__))
+
         self.id = id(self)
         self.exit_node = exit_node
         self.enter_node = enter_node
-        
+
     def __str__(self):
         return str(self.id)
+
 
 class Node(object):
     '''
     classdocs
     '''
-    
-    def __init__(self, obj = None):
+
+    def __init__(self, obj=None):
         '''
         Constructor
         :invariant: self.exit_degree >= 0.
@@ -270,32 +294,34 @@ class Node(object):
         :invariant: self.full_degree >= 0
         '''
         self.id = id(self)
-        self.data = self.id if obj == None else obj
+        self.data = self.id if obj is None else obj
         self.exit_edges = set()
         self.enter_edges = set()
-        
+
     def __str__(self):
         return str(self.id)
-    
-    @property            
+
+    @property
     def adjacencies_edges(self):
         '''
         Return node adjacencies edges, if exists.
         '''
         return self.exit_edges.union(self.enter_edges)
-    
+
     @property
     def exit_degree(self):
         '''
         Return node exit degree.
         '''
         return len(self.exit_edges)
+
     @property
     def enter_degree(self):
         '''
         Return node enter degree.
         '''
         return len(self.enter_edges)
+
     @property
     def degree(self):
         '''
--- Graphs/testing/test_graph.py
+++ Graphs/testing/test_graph.py
@@ -6,256 +6,266 @@ Created on Jan 27, 2018
 import pytest
 from components import graph
 
+
 class TestNode(object):
+
     @classmethod
     def setup_class(cls):
         cls.node0 = graph.Node(0)
         cls.node1 = graph.Node("My node")
-    
+
     def test_create_node(self):
         assert self.node1.id != self.node0.id
-        
-        assert 0 == self.node0.data
-        assert 0 == self.node0.exit_degree
-        assert 0 == self.node0.enter_degree
-        assert 0 == self.node0.degree
+
+        assert self.node0.data == 0
+        assert self.node0.exit_degree == 0
+        assert self.node0.enter_degree == 0
+        assert self.node0.degree == 0
         assert set() == self.node0.exit_edges
         assert set() == self.node0.enter_edges
         assert set() == self.node0.adjacencies_edges
-        
-        assert "My node" == self.node1.data
-        assert 0 == self.node1.exit_degree
-        assert 0 == self.node1.enter_degree
-        assert 0 == self.node1.degree
+
+        assert self.node1.data == "My node"
+        assert self.node1.exit_degree == 0
+        assert self.node1.enter_degree == 0
+        assert self.node1.degree == 0
         assert set() == self.node1.exit_edges
         assert set() == self.node1.enter_edges
         assert set() == self.node1.adjacencies_edges
 
-    @classmethod    
+    @classmethod
     def teardown_class(cls):
         cls.node0 = None
         cls.node1 = None
-        
+
+
 class TestEdge(object):
+
     @classmethod
     def setup_class(cls):
         cls.node0 = graph.Node(0)
         cls.node1 = graph.Node(1)
         cls.edge01 = graph.Edge(cls.node0, cls.node1)
         cls.edge10 = graph.Edge(cls.node1, cls.node0)
-    
+
     def test_create_edge(self):
         assert self.edge01 is not self.edge10
         assert self.edge01.id != self.edge10.id
-        
+
         assert self.node0 is self.edge01.exit_node
         assert self.node1 is self.edge01.enter_node
         assert self.node1 is self.edge10.exit_node
         assert self.node0 is self.edge10.enter_node
-    
-    @classmethod    
+
+    @classmethod
     def teardown_class(cls):
         cls.node0 = None
         cls.node1 = None
         cls.edge0 = None
         cls.edge1 = None
-        
-class TestGraph():
-    
+
+
+class TestGraph(object):
+
     def setup_method(self):
         self.graph0 = graph.Graph()
-        self.graph1 = graph.Graph([0,1],[(0,1),(1,0)])
+        self.graph1 = graph.Graph([0, 1], [(0, 1), (1, 0)])
 
     def test_nodes_data(self):
-        assert set() ==  self.graph0.nodes_data
-        assert set([0,1]) == self.graph1.nodes_data
-    
+        assert set() == self.graph0.nodes_data
+        assert set([0, 1]) == self.graph1.nodes_data
+
     def test_edges_data(self):
         assert set() == self.graph0.edges_data
-        assert set([(0,1) == (1,0)])
-        
+        assert set([(0, 1) == (1, 0)])
+
     def test_create_graph(self):
         graph1_nodes_data = self.graph1.nodes_data
         graph1_edges_data = self.graph1.edges_data
-   
+
         assert self.graph0.id != self.graph1.id
-        
+
         assert set() == self.graph0.nodes
         assert set() == self.graph0.edges
-        
-        assert set([0,1]) == graph1_nodes_data
-        assert set([(0,1),(1,0)]) == graph1_edges_data
-        
+
+        assert set([0, 1]) == graph1_nodes_data
+        assert set([(0, 1), (1, 0)]) == graph1_edges_data
+
     def test_get_node_by_obj(self):
         assert self.graph0.get_node_by_obj("No such node") is None
-        
+
         assert self.graph1.get_node_by_obj(None) is None
         assert self.graph1.get_node_by_obj("No such node") is None
-        assert type(self.graph1.get_node_by_obj(0)) is graph.Node
-        assert 0 == self.graph1.get_node_by_obj(0).data
-        
-        assert type(self.graph1.get_node_by_obj(1)) is graph.Node
-        assert 1 == self.graph1.get_node_by_obj(1).data
-    
+        assert isinstance(self.graph1.get_node_by_obj(0), graph.Node)
+        assert self.graph1.get_node_by_obj(0).data == 0
+
+        assert isinstance(self.graph1.get_node_by_obj(1), graph.Node)
+        assert self.graph1.get_node_by_obj(1).data == 1
+
     def test_get_edge_by_objs(self):
-        assert self.graph0.get_edge_by_objs("exit obj","enter obj") is None
-        
+        assert self.graph0.get_edge_by_objs("exit obj", "enter obj") is None
+
         assert self.graph1.get_edge_by_objs(None, None) is None
-        assert self.graph1.get_edge_by_objs(0,2) is None
-        assert self.graph1.get_edge_by_objs(2,0) is None
-        assert self.graph1.get_edge_by_objs(0,0) is None
-        
-        assert type(self.graph1.get_edge_by_objs(0,1)) is graph.Edge
-        assert 0 == self.graph1.get_edge_by_objs(0,1).exit_node.data
-        assert 1 == self.graph1.get_edge_by_objs(0,1).enter_node.data
-        
-        assert type(self.graph1.get_edge_by_objs(1,0)) is graph.Edge
-        assert 1 == self.graph1.get_edge_by_objs(1,0).exit_node.data
-        assert 0 == self.graph1.get_edge_by_objs(1,0).enter_node.data    
-            
+        assert self.graph1.get_edge_by_objs(0, 2) is None
+        assert self.graph1.get_edge_by_objs(2, 0) is None
+        assert self.graph1.get_edge_by_objs(0, 0) is None
+
+        assert isinstance(self.graph1.get_edge_by_objs(0, 1), graph.Edge)
+        assert self.graph1.get_edge_by_objs(0, 1).exit_node.data == 0
+        assert self.graph1.get_edge_by_objs(0, 1).enter_node.data == 1
+
+        assert isinstance(self.graph1.get_edge_by_objs(1, 0), graph.Edge)
+        assert self.graph1.get_edge_by_objs(1, 0).exit_node.data == 1
+        assert self.graph1.get_edge_by_objs(1, 0).enter_node.data == 0
+
     def test_add_node(self):
         node0 = self.graph0.add_node("My node")
         node1 = self.graph0.add_node(0)
         node2 = self.graph0.add_node(0)
-        
-        assert type(node0) is graph.Node
-        assert type(node1) is graph.Node
-        assert type(node2) is graph.Node
-        
+
+        assert isinstance(node0, graph.Node)
+        assert isinstance(node1, graph.Node)
+        assert isinstance(node2, graph.Node)
+
         assert node1 is node2
-        
+
         assert None not in self.graph0.nodes
         assert node0 in self.graph0.nodes
         assert node1 in self.graph0.nodes
-        
-        assert "My node" == node0.data
-        assert 0 == node1.data
-        assert 0 == node0.exit_degree
-        assert 0 == node1.exit_degree
-        assert 0 == node0.enter_degree
-        assert 0 == node1.enter_degree
-        assert 0 == node0.degree
-        assert 0 == node1.degree
+
+        assert node0.data == "My node"
+        assert node1.data == 0
+        assert node0.exit_degree == 0
+        assert node1.exit_degree == 0
+        assert node0.enter_degree == 0
+        assert node1.enter_degree == 0
+        assert node0.degree == 0
+        assert node1.degree == 0
         assert set() == node0.exit_edges
         assert set() == node1.exit_edges
         assert set() == node0.enter_edges
         assert set() == node1.enter_edges
         assert set() == node0.adjacencies_edges
         assert set() == node1.adjacencies_edges
-        
-        assert 2 == len(self.graph0.nodes)     
-    
-    def test_add_directed_edge(self):        
-        assert 0 == len(self.graph0.nodes)
-        
-        edge0 = self.graph0.add_directed_edge(0,1)
-        edge1 = self.graph0.add_directed_edge(1,0)
-        edge2 = self.graph0.add_directed_edge(1,0)
-        
-        assert type(edge0) is graph.Edge
-        assert type(edge1) is graph.Edge
-        assert type(edge2) is graph.Edge
-        
+
+        assert len(self.graph0.nodes) == 2
+
+    def test_add_directed_edge(self):
+        assert len(self.graph0.nodes) == 0
+
+        edge0 = self.graph0.add_directed_edge(0, 1)
+        edge1 = self.graph0.add_directed_edge(1, 0)
+        edge2 = self.graph0.add_directed_edge(1, 0)
+
+        assert isinstance(edge0, graph.Edge)
+        assert isinstance(edge1, graph.Edge)
+        assert isinstance(edge2, graph.Edge)
+
         assert edge1 is edge2
-        assert 2 == len(self.graph0.nodes)
-                
-        assert 0 == edge0.exit_node.data        
-        assert 1 == edge0.enter_node.data
-        assert 1 == edge1.exit_node.data        
-        assert 0 == edge1.enter_node.data
-        
+        assert len(self.graph0.nodes) == 2
+
+        assert edge0.exit_node.data == 0
+        assert edge0.enter_node.data == 1
+        assert edge1.exit_node.data == 1
+        assert edge1.enter_node.data == 0
+
         assert edge0 in edge0.exit_node.exit_edges
         assert edge1 in edge1.exit_node.exit_edges
         assert edge1 in edge0.exit_node.enter_edges
         assert edge0 in edge1.exit_node.enter_edges
         assert set([edge0, edge1]) == edge0.exit_node.adjacencies_edges
         assert set([edge0, edge1]) == edge1.exit_node.adjacencies_edges
-        
-        assert 1 == edge0.exit_node.exit_degree
-        assert 1 == edge1.exit_node.exit_degree
-        assert 1 == edge0.exit_node.enter_degree
-        assert 1 == edge1.exit_node.enter_degree
-        assert 2 == edge0.exit_node.degree
-        assert 2 == edge1.exit_node.degree
-    
+
+        assert edge0.exit_node.exit_degree == 1
+        assert edge1.exit_node.exit_degree == 1
+        assert edge0.exit_node.enter_degree == 1
+        assert edge1.exit_node.enter_degree == 1
+        assert edge0.exit_node.degree == 2
+        assert edge1.exit_node.degree == 2
+
     def test_add_nodes_from(self):
         nodes_bunch = self.graph0.add_nodes_from(range(10))
-        
-        assert 10 == len(self.graph0.nodes)
-        
+
+        assert len(self.graph0.nodes) == 10
+
         for node in nodes_bunch:
-            assert  type(node) is graph.Node
-    
+            assert  isinstance(node, graph.Node)
+
     def test_add_edges_from(self):
-        edges_bunch = self.graph0.add_edges_from(zip(range(10), range(10,20)))
-            
-        assert 20 == len(self.graph0.nodes)
-        assert 10 == len(self.graph0.edges)
-        
+        edges_bunch = self.graph0.add_edges_from(zip(range(10), range(10, 20)))
+
+        assert len(self.graph0.nodes) == 20
+        assert len(self.graph0.edges) == 10
+
         for edge in edges_bunch:
-            assert  type(edge) is graph.Edge
-    
+            assert  isinstance(edge, graph.Edge)
+
     @pytest.mark.skip(reason="No way of currently testing this.")
     def test_remove_edge(self):
         pass
-    
+
     @pytest.mark.skip(reason="No way of currently testing this.")
     def test_remove_edges_from(self):
         pass
-    
+
     def test_del_node(self):
         node0 = self.graph0.add_node(0)
-        node1 = self.graph0.add_node(1)        
-        assert 0 == node0.data
-        assert 1 == node1.data
-        assert 2 == len(self.graph0.nodes)
-        
+        node1 = self.graph0.add_node(1)
+        assert node0.data == 0
+        assert node1.data == 1
+        assert len(self.graph0.nodes) == 2
+
         node1 = self.graph0.del_node(1)
-        assert 1 == len(self.graph0.nodes)
-        assert 0 == self.graph0.nodes.pop().data
-    
+        assert len(self.graph0.nodes) == 1
+        assert self.graph0.nodes.pop().data == 0
+
     def test_del_edge(self):
         edge0 = self.graph0.add_directed_edge(0, 1)
-        edge0 = self.graph0.add_directed_edge(1, 2)        
-        assert 3 == len(self.graph0.nodes)
-        assert 2 == len(self.graph0.edges)
-        
-        edge0 = self.graph0.del_edge(1, 2)
-        assert 3 == len(self.graph0.nodes)
-        assert 1 == len(self.graph0.edges)
-    
+        edge1 = self.graph0.add_directed_edge(1, 2)
+        assert edge0.exit_node.data == 0
+        assert edge0.enter_node.data == 1
+        assert edge1.exit_node.data == 1
+        assert edge1.enter_node.data == 2
+        assert len(self.graph0.nodes) == 3
+        assert len(self.graph0.edges) == 2
+
+        edge1 = self.graph0.del_edge(1, 2)
+        assert edge1.exit_node.data == 1
+        assert edge1.enter_node.data == 2
+        assert len(self.graph0.nodes) == 3
+        assert len(self.graph0.edges) == 1
+
     def test_del_nodes_from(self):
-        nodes_bunch = self.graph0.add_nodes_from(range(10))
-        assert 10 == len(self.graph0.nodes)
-        
-        new_nodes_bunch = self.graph0.del_nodes_from(range(4))
-        assert 6 == len(self.graph0.nodes)
-        
-        new_nodes_bunch = self.graph0.del_nodes_from(range(4))
-        assert 6 == len(self.graph0.nodes)
-        
-        new_nodes_bunch = self.graph0.del_nodes_from(range(5,8))
-        assert 3 == len(self.graph0.nodes)    
-    
+        self.graph0.add_nodes_from(range(10))
+        assert len(self.graph0.nodes) == 10
+
+        self.graph0.del_nodes_from(range(4))
+        assert len(self.graph0.nodes) == 6
+
+        self.graph0.del_nodes_from(range(4))
+        assert len(self.graph0.nodes) == 6
+
+        self.graph0.del_nodes_from(range(5, 8))
+        assert len(self.graph0.nodes) == 3
+
     def test_del_edges_from(self):
-        edges_bunch = self.graph0.add_edges_from(zip(range(10), range(10,20)))
-        assert 20 == len(self.graph0.nodes)
-        assert 10 == len(self.graph0.edges)
-        
-        new_edges_bunch = self.graph0.del_edges_from(zip(range(4),range(10,14)))
-        assert 20 == len(self.graph0.nodes)
-        assert 6 == len(self.graph0.edges)
-        
-        new_edges_bunch = self.graph0.del_edges_from(zip(range(4),range(10,14)))
-        assert 20 == len(self.graph0.nodes)
-        assert 6 == len(self.graph0.edges)
-        
-        new_edges_bunch = self.graph0.del_edges_from(zip(range(5,8),range(15,18)))
-        assert 20 == len(self.graph0.nodes)
-        assert 3 == len(self.graph0.edges)
+        self.graph0.add_edges_from(zip(range(10), range(10, 20)))
+        assert len(self.graph0.nodes) == 20
+        assert len(self.graph0.edges) == 10
+
+        self.graph0.del_edges_from(zip(range(4), range(10, 14)))
+        assert len(self.graph0.nodes) == 20
+        assert len(self.graph0.edges) == 6
+
+        self.graph0.del_edges_from(zip(range(4), range(10, 14)))
+        assert len(self.graph0.nodes) == 20
+        assert len(self.graph0.edges) == 6
+
+        self.graph0.del_edges_from(zip(range(5, 8), range(15, 18)))
+        assert len(self.graph0.nodes) == 20
+        assert len(self.graph0.edges) == 3
 
     def teardown_method(self):
-        #super(TestGraph, self).tearDown()(self):
+        # super(TestGraph, self).tearDown()(self):
         self.graph0 = None
         self.graph1 = None
-
